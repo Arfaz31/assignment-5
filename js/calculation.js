cf895cc67c34832ed5ec0dcd6aca5d7b29c5d7da
ArfazAhamed <arfazahamed31@gmail.com>
2023-02-19 15:54:54 +0600

    validation-add

--- js/calculation.js
+++ js/calculation.js
@@ -1,71 +1,33 @@
 
-function getArea(baseValueId, heightValueId){
-    // for base value
-    const baseValue= document.getElementById(baseValueId);
-    const baseValueString= baseValue.value;
-    const baseValueNumber= parseFloat(baseValueString);
-    baseValue.value="";
-   
-    // for height value
-    const heightValue= document.getElementById(heightValueId);
-    const heightValueString= heightValue.value;
-    const heightValueNumber= parseFloat(heightValueString);
-    heightValue.value= "";
-    
-    // base and height multiplication
-       const areaCalTotalString= (0.5 * baseValueNumber * heightValueNumber).toFixed(2);
-       const areaCalTotal = parseFloat(areaCalTotalString);
-       return areaCalTotal;
-   
-   }
-   
-   
-   function getAreaValues(valueBaseId, valueHeightId){
-    // for base value
-    const baseValue= document.getElementById(valueBaseId);
-    const baseValueString= baseValue.value;
-    const baseValueNumber= parseFloat(baseValueString);
-    baseValue.value=""; 
-   
-    // for height value
-    const heightValue= document.getElementById(valueHeightId);
-    const heightValueString= heightValue.value;
-    const heightValueNumber= parseFloat(heightValueString);
-    heightValue.value= "";
-   
-    // base and height multiplication
-    const areaCalculationString= (baseValueNumber * heightVlueNumber).toFixed(2);
-    const areaCalculation= parseFloat(areaCalculationString);
-    return areaCalculation;
 
-   }
-   
-   let countNo = 0;
-function displayData(nameOfGeometry, calculateValue,){
-    countNo += 1;
-    const parent = document.getElementById('content-container');
-const tr = document.createElement('tr');
-tr.innerHTML = `
-<td>${countNo}</td>
-<td>${nameOfGeometry}</td>
-<td>${calculateValue} cm<sup>2</sup></td>
-`;
-
-parent.appendChild(tr);
-}
-   
    document.getElementById("triangle-calculate-btn").addEventListener("click", function(){
-       
+   
        const triangleTotal= getArea("triangle-base-value", "triangle-height-value")
-       //  show the value in area calculation
+   
+    //   show validation
+       const v = triangleTotal;
+       if(v < 0 || isNaN(v) === true || v == ''){
+        alert('Enter valid Number')
+        return v;
+       }
+        //  show the value in area calculation
        displayData('Triangle', triangleTotal)
+       
    })
    
    document.getElementById("rhombus-calculate-btn").addEventListener("click", function(){
    
     const  rhombusTotal= getArea("rhombus-base-value", "rhombus-height-value")
+
+    //   show validation
+    const valid = rhombusTotal;
+    if(valid < 0 || isNaN(valid) === true || valid == ''){
+     alert('Enter valid Number')
+     return v;
+    }
     //  show the value in area calculation
     displayData('Rhombus', rhombusTotal)
+    
 })
 
 
@@ -81,6 +43,13 @@ document.getElementById("pentagon-calculate-btn").addEventListener("click", func
    document.getElementById("rectangle-calculate-btn").addEventListener("click", function(){
    
        const  rectangleTotal= getAreaValues("rectangle-base-value", "rectangle-height-value")
+    
+    //   show validation
+    const validation = rectangleTotal;
+    if(validation < 0 || isNaN(validation) === true || validation == ''){
+     alert('Enter valid Number')
+     return validation;
+    }
        //  show the value in area calculation
        displayData('Rectangle', rectangleTotal)
    
---
+++ js/utility.js
@@ -0,0 +1,66 @@
+
+
+  function getArea(baseValueId, heightValueId){
+    // for base value
+ 
+    const baseValue= document.getElementById(baseValueId);
+    const baseValueString= baseValue.value;
+    const baseValueNumber= parseFloat(baseValueString);
+    baseValue.value="";
+    
+
+    // for height value
+    const heightValue= document.getElementById(heightValueId);
+    const heightValueString= heightValue.value;
+    const heightValueNumber= parseFloat(heightValueString);
+    heightValue.value= "";
+  
+
+    // base and height multiplication
+       const areaCalTotalString= (0.5 * baseValueNumber * heightValueNumber).toFixed(2);
+       const areaCalTotal = parseFloat(areaCalTotalString);
+       return areaCalTotal;
+
+   }
+
+   
+   function getAreaValues(valueBaseId, valueHeightId){
+    // for base value
+    const baseValue= document.getElementById(valueBaseId);
+    const baseValueString= baseValue.value;
+    const baseValueNumber= parseFloat(baseValueString);
+    baseValue.value=""; 
+
+    
+    // for height value
+    const heightValue= document.getElementById(valueHeightId);
+    const heightValueString= heightValue.value;
+    const heightValueNumber= parseFloat(heightValueString);
+    heightValue.value= "";
+   
+   
+    // base and height multiplication
+    const areaCalculationString= (baseValueNumber * heightValueNumber).toFixed(2);
+    const areaCalculation= parseFloat(areaCalculationString);
+    return areaCalculation;
+
+   }
+   
+   let countNo = 0;
+function displayData(nameOfGeometry, calculateValue,){
+    countNo += 1;
+    const parent = document.getElementById('content-container');
+const tr = document.createElement('tr');
+tr.innerHTML = `
+<td>${countNo}</td>
+<td>${nameOfGeometry}</td>
+<td>${calculateValue} cm<sup>2</sup></td>
+<td><button class="btn btn-primary fs-6 ">Convert m<sup>2</sup></button ></td>
+`;
+
+parent.appendChild(tr);
+}
+
+
+
+
